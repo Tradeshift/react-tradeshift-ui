0495999f8acfff0d6271282ec6d94cc09195770a
Kaue Machado <kaumac@gmail.com>
2017-06-14 16:22:32 +0200

    Implement StatusBar component

--- src/components/StatusBar/index.js
+++ src/components/StatusBar/index.js
@@ -1,22 +1,36 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// const noop = () => {};
-
 class StatusBar extends Component {
-	componentWillReceiveProps(nextProps) {
-		this.visible = nextProps.visible;
+	componentDidMount() {
+		this.update(this.props.visible, this.props.linkable, this.props.message, this.props.buttons);
+	}
 
-		if (nextProps.visible) {
-			if (nextProps.linkable) {
-				window.ts.ui.StatusBar.linkable();
-			}
-			window.ts.ui.StatusBar.message(nextProps.message);
-			window.ts.ui.StatusBar.buttons(nextProps.buttons);
-			window.ts.ui.StatusBar.show();
-		} else {
-			window.ts.ui.StatusBar.hide();
+	componentWillReceiveProps(nextProps) {
+		this.update(nextProps.visible, nextProps.linkable, nextProps.message, nextProps.buttons);
+	}
+
+	componentWillUnmount() {
+		if (this.bar) {
+			this.bar.hide();
 		}
+	}
+
+	update(visible, linkable, message, buttons) {
+		window.ts.ui.ready(() => {
+			this.bar = window.ts.ui.StatusBar;
+
+			if (visible) {
+				if (linkable) {
+					this.bar.linkable();
+				}
+				this.bar.message(message);
+				this.bar.buttons(buttons);
+				this.bar.show();
+			} else {
+				this.bar.hide();
+			}
+		});
 	}
 
 	render() {
